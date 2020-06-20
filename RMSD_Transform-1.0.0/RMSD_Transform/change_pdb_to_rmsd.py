f0321f14657b87586e95e2e6216196912971ab76
weixiang Wang <18829352615@163.com>
2020-06-20 21:03:23 +0800

    change format and fix bug about change_pdb_to_rmsd

--- RMSD_Transform-1.0.0/RMSD_Transform/change_pdb_to_rmsd.py
+++ RMSD_Transform-1.0.0/RMSD_Transform/change_pdb_to_rmsd.py
@@ -3,14 +3,16 @@ import collections
 import argparse
 import json
 
-def get_value_list_location(my_value,my_list):
-    my_loc=[]
+
+def get_value_list_location(my_value, my_list):
+    my_loc = []
     for item in enumerate(my_list):
         if item[1] == my_value:
             my_loc.append(item[0])
     return my_loc[-1]
 
-def change_rmsd_dic_range(rmsd_dic,times):
+
+def change_rmsd_dic_range(rmsd_dic, times):
     rmsd_mutiple_dic = collections.OrderedDict()
     for aa_number in rmsd_dic.keys():
         rmsd_value = float(rmsd_dic[aa_number])
@@ -18,41 +20,36 @@ def change_rmsd_dic_range(rmsd_dic,times):
     print("Multiple a number to RMSD...")
     return rmsd_mutiple_dic
 
-def get_max_rmsd(rmsd_dic,times,plus_value = 10):
-    rmsd_list = [float(i)*int(times) for i in list(rmsd_dic.values())]
+
+def get_max_rmsd(rmsd_dic, times, plus_value=10):
+    rmsd_list = [float(i) * int(times) for i in list(rmsd_dic.values())]
     rmsd_max = max(rmsd_list)
     rmsd_max_plus_value = round(rmsd_max + plus_value, 2)
     print("Add a number to Max RMSD...")
     return rmsd_max_plus_value
 
-def change_pdb(default_pdb_path, rmsd_mutiple_dic, rmsd_max_plus_value,rmsd_pdb_path):
-    with open(rmsd_pdb_path,"w") as rpp:
+
+def change_pdb(
+        default_pdb_path,
+        rmsd_mutiple_dic,
+        rmsd_max_plus_value,
+        rmsd_pdb_path):
+    with open(rmsd_pdb_path, "w") as rpp:
         with open(default_pdb_path) as dpp:
             for line in dpp:
                 if (line[0:4] == "ATOM" and len(line.split()) > 6):
-                    line_info = line.strip().split()
-                    if len(line_info[4]) == 1:
-                        aa_number = line_info[4] + "_" + line_info[3] + line_info[5]
+                    aa_number = line[21] + "_" + \
+                        line[17:20] + line[22:26].strip()
+                    if aa_number in rmsd_mutiple_dic.keys():
+                        rmsd_len = len(str(rmsd_mutiple_dic[aa_number]))
+                        mid_line = (6 - rmsd_len) * " " + \
+                            str(rmsd_mutiple_dic[aa_number])
+                        new_line = line[0:60] + mid_line + line[66:]
                     else:
-                        aa_number = line_info[4][0] + "_" + line_info[3] + line_info[4][1:]
-                    line_all = line.split(" ")
-                    bfactor_value = line_info[-2]
-                    bfactor_loc = get_value_list_location(bfactor_value, line_all)
-                    if len(bfactor_value) > 6:
-                        if aa_number in rmsd_mutiple_dic.keys():
-                            line_all[bfactor_loc] = line_all[bfactor_loc][0:-6] + \
-                                                    (6-len(str(rmsd_mutiple_dic[aa_number]))
-                                                     )*" " + str(rmsd_mutiple_dic[aa_number])
-                        else:
-                            line_all[bfactor_loc] = line_all[bfactor_loc][0:-6] + \
-                                                    (6-len(str(rmsd_max_plus_value))
-                                                     )*" " + str(rmsd_max_plus_value)
-                    else:
-                        if aa_number in rmsd_mutiple_dic.keys():
-                            pass
-                        else:
-                            pass
-                    new_line = " ".join(line_all)
+                        rmsd_len = len(str(rmsd_max_plus_value))
+                        mid_line = (6 - rmsd_len) * " " + \
+                            str(rmsd_max_plus_value)
+                        new_line = line[0:60] + mid_line + line[66:]
                     rpp.write(new_line)
                 elif line[0:6] == "ANISOU":
                     pass
@@ -61,22 +58,52 @@ def change_pdb(default_pdb_path, rmsd_mutiple_dic, rmsd_max_plus_value,rmsd_pdb_
     print("change pdb B-factor to RMSD...")
     print("Finish Mission !")
 
-def change_pdb_all(rmsd_dic,times,default_pdb_path, rmsd_pdb_path,plus_value = 10):
+
+def change_pdb_all(
+        rmsd_dic,
+        times,
+        default_pdb_path,
+        rmsd_pdb_path,
+        plus_value=10):
     rmsd_mutiple_dic = change_rmsd_dic_range(rmsd_dic, times)
     rmsd_max_plus_value = get_max_rmsd(rmsd_dic, times, plus_value)
-    change_pdb(default_pdb_path, rmsd_mutiple_dic, rmsd_max_plus_value,rmsd_pdb_path)
+    change_pdb(
+        default_pdb_path,
+        rmsd_mutiple_dic,
+        rmsd_max_plus_value,
+        rmsd_pdb_path)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="This module is for change pdb b-factor to RMSD")
-    parser.add_argument("-j", "--rmsd_json", help="-j /../rmsd.json: The path of rmsd json,default:rmsd.json",
-                        default="rmsd.json")
-    parser.add_argument("-t", "--times", help="-t times: The times you want to mutiple RMSD,default:40",
-                        default=40, type=int)
-    parser.add_argument("-p", "--plus_value", help="-p plus_value:plus value to max RMSD,default:10",
-                        default=10, type=int)
-    parser.add_argument("-d", "--default_pdb", help="-d /../default.pdb: The initial PDB", required=True)
-    parser.add_argument("-f", "--rmsd_pdb", help="-f /../rmsd.pdb: The path of final rmsd pdb,default:rmsd.pdb",
-                        default="rmsd.pdb")
+    parser = argparse.ArgumentParser(
+        description="This module is for change pdb b-factor to RMSD")
+    parser.add_argument(
+        "-j",
+        "--rmsd_json",
+        help="-j /../rmsd.json: The path of rmsd json,default:rmsd.json",
+        default="rmsd.json")
+    parser.add_argument(
+        "-t",
+        "--times",
+        help="-t times: The times you want to mutiple RMSD,default:40",
+        default=40,
+        type=int)
+    parser.add_argument(
+        "-p",
+        "--plus_value",
+        help="-p plus_value:plus value to max RMSD,default:10",
+        default=10,
+        type=int)
+    parser.add_argument(
+        "-d",
+        "--default_pdb",
+        help="-d /../default.pdb: The initial PDB",
+        required=True)
+    parser.add_argument(
+        "-f",
+        "--rmsd_pdb",
+        help="-f /../rmsd.pdb: The path of final rmsd pdb,default:rmsd.pdb",
+        default="rmsd.pdb")
     options = parser.parse_args()
     rmsd_json = options.rmsd_json
     js = open(rmsd_json)
@@ -87,6 +114,8 @@ if __name__ == "__main__":
     rmsd_pdb_path = options.rmsd_pdb
     rmsd_mutiple_dic = change_rmsd_dic_range(rmsd_dic, times)
     rmsd_max_plus_value = get_max_rmsd(rmsd_dic, times, plus_value)
-    change_pdb(default_pdb_path, rmsd_mutiple_dic, rmsd_max_plus_value, rmsd_pdb_path)
-
-
+    change_pdb(
+        default_pdb_path,
+        rmsd_mutiple_dic,
+        rmsd_max_plus_value,
+        rmsd_pdb_path)
--- RMSD_Transform-1.0.0/RMSD_Transform/get_rmsd_value.py
+++ RMSD_Transform-1.0.0/RMSD_Transform/get_rmsd_value.py
@@ -3,30 +3,49 @@ import argparse
 import json
 import collections
 
-def get_rmsd_value(result_log,rmsd_json):
+
+def get_rmsd_value(result_log, rmsd_json):
     rmsd_dic = collections.OrderedDict()
     with open(result_log) as rl:
         for line in rl:
             if line.count("|") == 4:
                 chunk_info = line.split("|")
-                if (chunk_info[1].strip() and chunk_info[2].strip() and
-                        chunk_info[3].strip() and chunk_info[1].strip() != "Query"):
+                if (chunk_info[1].strip() and chunk_info[2].strip(
+                ) and chunk_info[3].strip() and chunk_info[1].strip() != "Query"):
                     aa_info = chunk_info[1].strip().split(":")
-                    aa_number = aa_info[0][-1] + "_" + "".join(aa_info[1].split())
+                    aa_number = aa_info[0][-1] + "_" + \
+                        "".join(aa_info[1].split())
                     aa_rmsd = chunk_info[2].strip().split()[1][0:4]
                     rmsd_dic[aa_number] = aa_rmsd
-    with open(rmsd_json,"w") as rmsd_js:
-        rmsd_js.write(json.dumps(rmsd_dic, ensure_ascii=False, indent=4, separators=(',', ':')))
+    with open(rmsd_json, "w") as rmsd_js:
+        rmsd_js.write(
+            json.dumps(
+                rmsd_dic,
+                ensure_ascii=False,
+                indent=4,
+                separators=(
+                    ',',
+                    ':')))
     print("Get RMSD !")
     print("Save RMSD to " + rmsd_json)
     return rmsd_dic
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="This module is for get rmsd value")
-    parser.add_argument("-l", "--log_file", help="-l /../result.log: The path of result log,"
-                                                 "default:result.log",default="result.log")
-    parser.add_argument("-j", "--rmsd_json", help="-j /../rmsd.json: The path of rmsd json,"
-                                                  "default:rmsd.json",default="rmsd.json")
+    parser = argparse.ArgumentParser(
+        description="This module is for get rmsd value")
+    parser.add_argument(
+        "-l",
+        "--log_file",
+        help="-l /../result.log: The path of result log,"
+        "default:result.log",
+        default="result.log")
+    parser.add_argument(
+        "-j",
+        "--rmsd_json",
+        help="-j /../rmsd.json: The path of rmsd json,"
+        "default:rmsd.json",
+        default="rmsd.json")
     options = parser.parse_args()
     result_log = options.log_file
     rmsd_json = options.rmsd_json
